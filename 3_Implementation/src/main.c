cc687b6bb1df94b90214747df157b8f214f482df
PoosaAnusha <89648119+PoosaAnusha@users.noreply.github.com>
2021-09-08 11:52:38 +0530

    Add files via upload

---
+++ 3_Implementation/src/main.c
@@ -0,0 +1,16 @@
+#include"system.h"
+void main()
+{
+
+  int i;
+
+  //printf("%s\n", "###### Leading Candidate ######");
+
+  startup();
+
+  for (i = 0; i < 5; i++)
+  {
+    temp[i].Vote = 0;
+    // printf("%s\t%d\n", name[i], temp[i].Vote);
+  }
+}
---
+++ 6_Image and Videos/README.md
@@ -0,0 +1,12 @@
+# IMAGES ADDED:
+ 1.Caste the vote.
+ 
+ 2.Choose the candidate.
+ 
+ 3.Find the vote count.
+ 
+ 4.Leading Candidate.
+ 
+ 
+# VIDEO ADDED:
+ 1.IMPLEMENTATION VIDEO.
